e5bee3f1cfb5a0bb7f3f676841f8db1554972ed4
MaazTariq409 <maaztariq409@gmail.com>
2023-05-24 15:42:23 +0500

    Image Upload Controller created

---
+++ Controllers/ImageUploadController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Portfolio_API.Controllers
+{
+    [Route("api/uploadimage")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHost;
+
+        public ImageUploadController(IWebHostEnvironment webHost)
+        {
+            _webHost = webHost;
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile file)
+        {
+            string wwwrootpath = _webHost.WebRootPath;
+            if (file != null)
+            {
+                string filename = Guid.NewGuid().ToString();
+                var Uploadpath = Path.Combine(wwwrootpath, @"images\");
+                var extension = Path.GetExtension(file.FileName);
+
+                //if (obj.Product.ImageUrl != null)
+                //{
+                //    var oldImagePath = Path.Combine(wwwrootpath, obj.Product.ImageUrl.TrimStart('\\'));
+                //    if (System.IO.File.Exists(oldImagePath))
+                //    {
+                //        System.IO.File.Delete(oldImagePath);
+                //    }
+                //}
+
+                var fileStream = new FileStream(Path.Combine(Uploadpath, filename + extension), FileMode.Create);
+
+                file.CopyTo(fileStream);
+
+                return Ok(new { imageUrl = @"images\" + filename + extension });
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
